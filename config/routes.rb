67807b2cc27d573a61dd56525365b06a00824da0
EC2 Default User <ec2-user@ip-172-31-16-67.us-east-2.compute.internal>
2020-03-24 07:38:11 +0000

    ユーザーのプロフィールを更新機能追加終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'maps#home'
 
-  get '/about', to: 'maps#about'
-  get '/signup', to: 'users#new'
+  get  '/about', to: 'maps#about'
+  get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
 resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200317085731) do
+ActiveRecord::Schema.define(version: 20200324071223) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "address"
     t.string   "phone_number"
-    t.integer  "password_digest"
+    t.string   "password_digest"
     t.integer  "admin"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
   end
 
 end
